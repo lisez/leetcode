f864d621435843f81ba62fde3996dabec801beb4
lisez <mm4324@gmail.com>
2023-12-03 23:15:38 +0800

    update

--- adventofcode/2023/day3.ts
+++ adventofcode/2023/day3.ts
@@ -8,7 +8,7 @@ async function readFile(): Promise<string> {
 function symbolized(c: string): number {
   switch (true) {
     case /\d/.test(c):
-      return 1;
+      return 2;
     case c === ".":
       return 0;
     default:
@@ -16,30 +16,77 @@ function symbolized(c: string): number {
   }
 }
 
-function makeFakeLine(len: number): string {
-  return "0".repeat(len);
+function* iteratePairs(length: number) {
+  for (let i = 0; i < length - 1; i += 1) {
+    yield [i, i + 1] as [number, number];
+  }
 }
 
-function* twoStepMove(line: string) {
-  for (let i = 0; i < line.length - 1; i += 1) {
-    const one = line[i];
-    const two = line[i + 1];
-    yield [{ index: i, value: +one }, { index: i + 1, value: +two }];
-  }
+function validator(nums: number[]): boolean {
+  return nums.includes(2) && nums.some((e) => e === 1);
+}
+
+function findAllNumbers(line: string) {
+  const matches = line.matchAll(/\d+/g);
+  return [...matches].map((m) => (
+    {
+      num: +m[0],
+      start: m.index!,
+      end: m.index! + m[0].length - 1,
+    }
+  ));
 }
 
 async function main() {
   const text = await readFile();
-  const map = text.split("\n").map((line) =>
+  const lines = text.split("\n");
+  const map: string[] = lines.map((line) =>
     line.split("").map(symbolized).join("")
   );
 
-  for (let line = 0; line < map.length; line++) {
-    // const next = map[line + 1] || makeFakeLine(map[line].length);
-    for (const moves of twoStepMove(map[line])) {
-      console.log(moves);
+  const records: Map<number, Set<number>> = new Map();
+  for (const [line1, line2] of iteratePairs(map.length)) {
+    const line = map[line1];
+    const nextLine = map[line2];
+
+    for (const [char1, char2] of iteratePairs(line.length)) {
+      const chars1 = [+line[char1], +line[char2]];
+      const chars2 = [+nextLine[char1], +nextLine[char2]];
+      const result = validator([...chars1, ...chars2]);
+
+      if (result) {
+        const val1 = records.get(line1) ?? new Set();
+        if (chars1[0] === 2) val1.add(char1);
+        if (chars1[1] === 2) val1.add(char2);
+        records.set(line1, val1);
+
+        const val2 = records.get(line2) ?? new Set();
+        if (chars2[0] === 2) val2.add(char1);
+        if (chars2[1] === 2) val2.add(char2);
+        records.set(line2, val2);
+      }
     }
   }
+
+  let result = 0;
+  for (const [key, val] of records.entries()) {
+    const line = lines[key];
+    const numbers = findAllNumbers(line);
+
+    const matches = numbers.filter((n) => {
+      const sorted = [...val].sort((a, b) => a - b);
+
+      for (const i of sorted) {
+        if (i >= n.start && i <= n.end) return true;
+      }
+      return false;
+    });
+
+    const sum = matches.reduce((acc, curr) => acc + curr.num, 0);
+    result += sum;
+  }
+
+  console.log(result);
 }
 
 main();
