9cf0e10609beaea5623ec83548b89096a8ee2ff5
lisez <mm4324@gmail.com>
2024-06-15 16:10:56 +0800

    chore: update leetcode

--- ecmascript/383_Ransom_Note.ts
+++ ecmascript/383_Ransom_Note.ts
@@ -1,18 +1,18 @@
-function freq(s: string): Map<string, number> {
-  const m = new Map<string, number>();
-  for (const c of s) {
-    m.set(c, (m.get(c) || 0) + 1);
-  }
-  return m;
+function freq(s: string): number[] {
+  return s.split("").reduce((acc, c) => {
+    acc[c.charCodeAt(0) - 97] += 1;
+    return acc;
+  }, Array(26).fill(0) as number[]);
 }
 
 export function canConstruct(ransomNote: string, magazine: string): boolean {
   const f = freq(magazine);
   for (const c of ransomNote) {
-    if (f.get(c) === 0 || !f.has(c)) {
+    const i = c.charCodeAt(0) - 97;
+    if (f[i] === 0) {
       return false;
     }
-    f.set(c, f.get(c)! - 1);
+    f[i] -= 1;
   }
   return true;
 }
